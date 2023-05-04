3b5b170f86a613e081eb114b55383e6742cc62ed
chetanakkatangerhal <78865519+chetanakkatangerhal@users.noreply.github.com>
2023-05-04 16:13:06 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() => runApp(CounterApp());
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: BlocProvider(
+        create: (_) => CounterCubit(),
+        child: CounterPage(),
+      ),
+    );
+  }
+}
+
